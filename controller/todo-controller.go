94d7955b763032d3b82d65da55407e3a478b2f5b
vcnt72 <vincentsanjaya72@gmail.com>
2020-12-17 14:26:05 +0700

    Refactoring controller

--- controller/todo-controller.go
+++ controller/todo-controller.go
@@ -1,14 +1,17 @@
 package controller
 
 import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"github.com/yet-another-todo-list-golang/model/dto"
-	"github.com/yet-another-todo-list-golang/model/entity"
 	"github.com/yet-another-todo-list-golang/service"
 )
 
+//TodoController is controller for todo
 type TodoController interface {
-	FindAll() []entity.Todo
-	Create(createDto *dto.CreateTodoDto)
+	FindAll(c *gin.Context)
+	Create(c *gin.Context)
 }
 
 type todoController struct {
@@ -24,10 +27,18 @@ func NewTodoController() TodoController {
 	}
 }
 
-func (todoController *todoController) FindAll() []entity.Todo {
-	return todoController.todoService.FindAll()
+func (todoController *todoController) FindAll(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Success",
+		"data":    todoController.todoService.FindAll(),
+	})
 }
 
-func (todoController *todoController) Create(createDto *dto.CreateTodoDto) {
-	todoController.todoService.Create(*createDto)
+func (todoController *todoController) Create(c *gin.Context) {
+	var createDto dto.CreateTodoDto
+	c.ShouldBindJSON(&createDto)
+	todoController.todoService.Create(createDto)
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "Success",
+	})
 }
