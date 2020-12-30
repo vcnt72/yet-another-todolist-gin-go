c521140c172e2616f2dbd0a3e9bc574175387fb2
vcnt72 <vincentsanjaya72@gmail.com>
2020-12-30 16:22:46 +0700

    Add test for updating todo

--- service/todo_service_test.go
+++ service/todo_service_test.go
@@ -171,7 +171,70 @@ func TestTodoCreate(t *testing.T) {
 }
 
 func TestTodoUpdate(t *testing.T) {
+	todoRepository := new(mocks.TodoRepository)
+	testCases := []struct {
+		name       string
+		input      dto.UpdateTodoDto
+		paramId    string
+		findOneErr error
+		findOneRet entity.Todo
+		updateErr  error
+		expected   error
+	}{
+		{
+			name: "Update todo",
+			input: dto.UpdateTodoDto{
+				Name:        "TodoUpdate",
+				Description: "TodoUpdate",
+				Status:      "COMPLETE",
+			},
+			paramId:    todos[2].ID,
+			expected:   nil,
+			findOneRet: todos[2],
+			findOneErr: nil,
+			updateErr:  nil,
+		},
+		{
+			name:       "Record not found",
+			input:      dto.UpdateTodoDto{},
+			paramId:    "3986041b-5533-4064-87f6-94db83575926", // random uuid update
+			expected:   errors.New("record not found"),
+			findOneRet: entity.Todo{},
+			findOneErr: errors.New("record not found"),
+			updateErr:  nil,
+		},
+		{
+			name:       "Database error",
+			input:      dto.UpdateTodoDto{},
+			paramId:    todos[2].ID,
+			expected:   errors.New("database error"),
+			updateErr:  errors.New("database error"),
+			findOneErr: nil,
+			findOneRet: todos[2],
+		},
+	}
 
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			todoRepository.On("FindOne", tc.paramId).Return(tc.findOneErr, tc.findOneRet)
+			todo := tc.findOneRet
+			if tc.findOneErr == nil {
+				todo.Name = tc.input.Name
+				todo.Description = tc.input.Description
+				todo.Status = tc.input.Status
+			}
+			todoRepository.On("Update", todo).Return(tc.updateErr)
+			todoService := service.NewTodoService(todoRepository)
+			err := todoService.Update(tc.paramId, tc.input)
+
+			if err != nil {
+				assert.EqualError(t, tc.expected, err.Error())
+				return
+			}
+
+			assert.Equal(t, tc.expected, nil)
+		})
+	}
 }
 
 func TestTodoDelete(t *testing.T) {
